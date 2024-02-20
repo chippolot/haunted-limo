0763fb42a4b00e517df31e4ae28365d70590bed1
Ben Smith <chippolot@gmail.com>
2024-02-20 13:33:31 -0800

    Try again w/ data

--- api/blunders.go
+++ api/blunders.go
@@ -3,7 +3,6 @@ package api
 import (
 	"html/template"
 	"net/http"
-	"path/filepath"
 
 	common "github.com/chippolot/haunted-limo/api/_pkg"
 	blunders "github.com/chippolot/haunted-limo/api/_pkg/blunders"
@@ -21,7 +20,7 @@ func Blunders(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	tmplPath, _ := filepath.Abs("data/templates/blunders.html")
+	tmplPath := "data/templates/blunders.html"
 	tmpl := template.Must(template.ParseFiles(tmplPath))
 	tmpl.Execute(w, result)
 }
--- api/index.go
+++ api/index.go
@@ -3,11 +3,10 @@ package api
 import (
 	"html/template"
 	"net/http"
-	"path/filepath"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	tmplPath, _ := filepath.Abs("data/templates/index.html")
+	tmplPath := "data/templates/index.html"
 	tmpl := template.Must(template.ParseFiles(tmplPath))
 	tmpl.Execute(w, nil)
 }
