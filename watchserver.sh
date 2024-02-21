a6d8ea936f4bbca1e5b58a6c390a7bef8f5e328d
Ben Smith <chippolot@gmail.com>
2024-02-21 11:48:04 -0800

    Fixing watch server command. Adding more bottom padding for stories

--- watchserver.sh
+++ watchserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 case "$OSTYPE" in
-  darwin*|linux*) nodemon --delay 1.5 -x "go run ./cmd/server || exit 1" --signal SIGTERM -e go,html ;;
-  msys*|cygwin*) nodemon --delay 1.5 -x "go run ./cmd/server || exit 1" --signal SIGKILL -e go,html ;;
+  darwin*|linux*) nodemon --delay 1.5 -x "go run ./cmd/server || exit 1" --signal SIGTERM -e go,html,gohtml ;;
+  msys*|cygwin*) nodemon --delay 1.5 -x "go run ./cmd/server || exit 1" --signal SIGKILL -e go,html,gohtml ;;
   *)        echo "Cannot start script with unknown OSTYPE: $OSTYPE" ;;
 esac
